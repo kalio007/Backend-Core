35d071a12a8fbc63fbe3e8a49951dbfda546e07d
kalio007 <kalioprince1@gmail.com>
2023-11-07 23:13:14 +0100

    hashed the users password

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,29 @@
 const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
 exports.register = (req, res, next) => {
     const { username, password, role } = req.body
     if (password.length < 6) {
         return res.status(400).json({ message: "Password less than 6 characters" })
     }
-    try {
-        User.create({
+    bcrypt.hash(password, 10).then(async (hash) => {
+        await User.create({
             username: username,
-            password: password,
+            password: hash,
             role: role,
-        }).then(user =>
-            res.status(200).json({
-                message: "User successfully created",
-                user,
-            })
-        )
-    } catch (err) {
-        res.status(401).json({
-            message: "User not successful created",
-            error: error.mesage,
         })
-    }
+            .then((user) =>
+                res.status(200).json({
+                    message: "User successfully created",
+                    user,
+                })
+            )
+            .catch((error) =>
+                res.status(400).json({
+                    message: "User not successful created",
+                    error: error.message,
+                })
+            );
+    });
+
 }
