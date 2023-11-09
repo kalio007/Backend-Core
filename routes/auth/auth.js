822679e4e927425363bcec5f85611c061509bf48
kalio007 <kalioprince1@gmail.com>
2023-11-09 23:56:09 +0100

    hashed user password, currently testing

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { register } = require("../../controllers/user");
+const { login } = require("../../controllers/login");
 
 router.get('/', (req, res) => {
     res.send('Hello World!')
 })
 router.post('/register', register)
+router.post('/login', login)
 
 module.exports = router;
